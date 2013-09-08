5c832c0fb57881cda6b0d81f3ff740b5523aece1
Natalia Ossipova <natalia.ossipova@gmail.com>
2013-09-08 23:16:08 +0200

    Initial Commit

---
+++ LanguageEditor/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  LanguageEditor
+//
+//  Created by Natalia Ossipova on 08.09.2013.
+//  Copyright (c) 2013 Natalia Ossipova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
